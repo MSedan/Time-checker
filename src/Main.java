3891854ef7c7956ff978a2cd4f0d8f9ecc9e5ef8
MSedan <merily.takel@gmail.com>
2026-08-11 19:23:03 +0100

    Creation of time validation and user input

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
+        // Setting up input
+        System.out.println("Enter start time");
+        Scanner input = new Scanner(System.in);
+        String startTime = input.nextLine();
+        System.out.println("Enter end time");
+        String endTime = input.nextLine();
+
+        System.out.println("Start time " + isValidTime(startTime));
+        System.out.println("Start time " + isValidTime(endTime));
+    }
+
+
+    private static final Pattern Time = Pattern.compile("^([01]\\d|2[0-3]):(00|15|30|45)$");
+
+    public static boolean isValidTime(String time) {
+        if (time == null) return false;
+        return Time.matcher(time).matches();
     }
 }
